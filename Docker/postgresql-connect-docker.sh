53fca191d6eb311dff819c22223e161ee5ede941
hurd-x0 <hurd@tutanota.com>
2020-09-06 20:46:39 +0430

    my script for bash

---
+++ Docker/postgresql-connect-docker.sh
@@ -0,0 +1,16 @@
+#!/bin/env bash
+
+#####################################################################
+
+# psql -h 127.0.0.1 -U postgres -p
+
+# psql -h 127.0.0.1 -U postgres 
+
+# psql -h <host> -p <port> -u <database>
+# psql -h <host> -p <port> -U <username> -W <password> <database>
+
+#####################################################################
+
+
+psql -h 172.17.0.2 -U postgres
+
